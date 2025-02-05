a72a655fa2b32b2853d1d6d491d2aae1aa536d69
Sayeed <expsayeedshorif@gmail.com>
2025-02-05 20:33:33 +0600

    added the basic designing part

--- src/index.js
+++ src/index.js
@@ -1,1 +1,19 @@
 import style from './style.css'
+import { format } from "date-fns";
+
+
+const modal = document.querySelector('.modal')
+let date = new Date();
+let newDate = format(date, 'dd/MM/yyyy');
+
+const detailsBtn = document.querySelector('.details')
+const modalCloseBtn = document.querySelector('.modal-close')
+const dateSpan = document.querySelector('.date-span');
+
+detailsBtn.addEventListener('click',()=>{
+    dateSpan.textContent = `${newDate}`
+    modal.showModal();
+})
+modalCloseBtn.addEventListener('click',()=>{
+    modal.close();
+})
