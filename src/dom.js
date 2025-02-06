a86e1d33c149d0e0598866b26f07c61ce0b026e2
Sayeed <expsayeedshorif@gmail.com>
2025-02-06 08:58:06 +0600

    added the javascript for project adding

---
+++ src/dom.js
@@ -0,0 +1,19 @@
+const projectCatalog = document.querySelector('.project-catalog')
+
+export default function createProject(name){
+    const div = document.createElement('div')
+    div.classList.add('project-inner');
+    div.innerHTML = `
+    <div class="project-icon">
+                            <i class="fa-solid fa-user-check"></i>
+                        </div>
+                        <p class="project-name">${name}</p>
+    `
+    appendChild(div, projectCatalog)
+
+}
+
+function appendChild(child, parent){
+    parent.appendChild(child);
+}
+
--- src/index.js
+++ src/index.js
@@ -1,5 +1,41 @@
 import style from './style.css'
 import { format } from "date-fns";
+import createProject from './dom';
+
+
+
+// project add 
+
+const addProjectBtn = document.querySelector('.add-project');
+const getInputProject = document.querySelector('.get-input-project');
+const addProjectNameBtn = document.querySelector('.add-project-btn')
+const cancelProjectNameBtn = document.querySelector('.cancel-project-btn')
+
+addProjectBtn.addEventListener('click',()=>{
+    addProjectBtn.classList.add('hidden')
+    getInputProject.classList.remove('hidden')
+})
+
+addProjectNameBtn.addEventListener('click',()=>{
+    let projectNameInput =  document.querySelector('#project-name-input')
+    let value = projectNameInput.value;
+    projectNameInput.value = ''
+    
+    if (value.trim() === '') {
+        alert('project name cant be blank')
+        return;
+    }
+    createProject(value);
+   
+})
+
+cancelProjectNameBtn.addEventListener('click',()=>{
+    addProjectBtn.classList.remove('hidden')
+    getInputProject.classList.add('hidden')
+})
+
+
+
 
 
 const modal = document.querySelector('.modal')
